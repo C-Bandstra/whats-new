2b3e07e48150a46301b7cd0574b7e25be0b766f1
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-05 10:29:14 -0600

    Edit app state, render menu and search form

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,31 @@
 import React, { Component } from 'react';
 import local from '../../data/local';
+import technology from '../../data/technology';
+import entertainment from '../../data/entertainment';
+import science from '../../data/science';
+import health from '../../data/health';
 import './App.css';
+import Menu from '../Menu/Menu'
+import SearchForm from '../SearchForm/SearchForm'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      local
+      allArticles: {local, technology, entertainment, science, health},
+      selected: local,
     }
+  }
+
+  updateSelected(topic) {
+    this.setState({selected: topic})
   }
 
   render () {
     return (
       <div className="app">
-        YOUR CODE GOES HERE!
+        <Menu topics={this.state.allArticles}/>
+        <SearchForm />
       </div>
     );
   }
