4c22147e434e044de4fecf742726d392dc7d898c
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-05 10:41:45 -0600

    Add SearchForm Component

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -3,4 +3,13 @@ import './SearchForm.css';
 
 // SEARCHFORM COMPONENT CODE GOES HERE
 
+const SearchForm = ()  => {
+  return (
+    <header className="main-header">
+      <input className="search-input" type="search" placeholder="Search for news articles here."></input>
+      <button className="search-btn">Search Now</button>
+    </header>
+  )
+}
+
 export default SearchForm;
