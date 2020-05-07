4c35a3f6b79e3e2a1b796db7ccb5cbdc5f4c9f53
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-07 09:22:28 -0600

    Add search functionality for form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './SearchForm.css';
 
 // SEARCHFORM COMPONENT CODE GOES HERE
 
-const SearchForm = ()  => {
-  return (
-    <header className="main-header">
-      <input className="search-input" type="search" placeholder="Search for news articles here."></input>
-      <button className="search-btn">Search Now</button>
-    </header>
-  )
+class SearchForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      value: ''
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.search(this.state.value)
+  }
+
+  render() {
+    return (
+      <header className="main-header">
+        <input onChange={this.handleChange} className="search-input" type="search" placeholder="Search for news articles here."></input>
+        <button onClick={this.handleSubmit} className="search-btn">Search Now</button>
+      </header>
+    )
+  }
 }
+
+
+
+
+
 
 export default SearchForm;
