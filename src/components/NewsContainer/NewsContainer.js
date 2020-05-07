3a5a2ee3af61dd64dcf00231133a9ded2edbde50
Charlie Bandstra <bandstracharlie@gmail.com>
2020-05-07 09:23:09 -0600

    Add dynamic functionality for display

--- src/components/NewsContainer/NewsContainer.js
+++ src/components/NewsContainer/NewsContainer.js
@@ -1,6 +1,26 @@
 import React from 'react';
 import './NewsContainer.css'
+import NewsArticle from '../NewsArticle/NewsArticle'
 
 // NEWSCONTAINER COMPONENT CODE GOES HERE
+
+const NewsContainer = (props) => {
+  let arr = props.articles
+  if(props.results.length) {
+    arr = props.results
+  }
+  
+  let articles = arr.map(article => {
+    return (
+      <NewsArticle key={article.id.toString()} info={article}/>
+    )
+  })
+
+  return (
+    <section className="news-container" key={props}>
+      {articles} 
+    </section>
+  )
+}
 
 export default NewsContainer;
